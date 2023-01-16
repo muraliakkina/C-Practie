33e2d4de6bec40db97fff6e8201370ca1893c5d0
Murali Akkina <muraliakkina@outlook.com>
2023-01-16 22:20:52 +0530

    latest

--- reverse_number.cpp
+++ reverse_number.cpp
@@ -13,6 +13,58 @@ void reverse_number(int n){
     }
 }
 
+void palindrome(int x){
+    int num = x;
+    int ans = 0 ;
+    while (num!=0)
+    {
+        ans = ans*10 + num%10;
+        num = num%10;
+        
+    }
+    cout << ans;
+    
+}
+
+//GCD of two numbers
+void gcd(int num1, int num2){
+    if(num1%num2 == 0 && num2%num2 == 0){
+        cout << num2;
+    }
+    else{
+        cout << num1;
+    }
+}
+
+//to check amstrong number
+void amstrong(int num){
+
+}
+
+// print all divisors
+void printAllDivisors(int n){
+    for(int i = 1; i<=n;i++){
+        if(n%i == 0){
+            cout << i << " ";
+        }
+    }
+}
+
+// CHeck for prime number
+void checkPrime(int num){
+    if(num%2 == 0 || num%3 == 0 || num%5 == 0 || num%7 == 0 || sqrt(num)){
+        cout << "Not a prime number";
+    }
+    else{
+        cout << "It is a prime number";
+    }
+}
+
+
 int main(){
-    reverse_number(95184);
+    // reverse_number(95184);
+    // palindrome(121);
+    // gcd(4,2);
+    // printAllDivisors(100);
+    checkPrime(169);
 }
