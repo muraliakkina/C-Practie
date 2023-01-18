f7b335ebc70e96ce8aff17ea668b6d71039a3ab9
Murali Akkina <muraliakkina@outlook.com>
2023-01-18 23:39:03 +0530

    recursion latest

--- recursion.cpp
+++ recursion.cpp
@@ -35,13 +35,39 @@ int nFactorial(int n){
     return n*nFactorial(n-1);
 }
 
+string checkPalindrome(string s){
+    // cout << char(tolower('A'));
+    // cout << char(91);
+    // for 0-9 ascii values 48-57
+    // for A - Z 65 - 90
+    // for a - z 97 - 122
+    // char split[s.length()];
+    string a;
+    for(int i = 0; i<s.length();i++){
+        if((tolower(s[i]) >= 97 && tolower(s[i]) <= 122)||(tolower(s[i]) >= 48 && tolower(s[i])<=57)){
+            a += char(tolower(s[i]));
+        }
+    }
+
+    string b;
+
+    for(int i=a.length()-1;i>=0;i--){
+        b += a[i];
+    }
+
+    // if(a == b) return true;
+    return b;
+}
+
 
 int main(){
     // nTimesReverse(5);
     // nTimes(5);
-    int ans = sumOfNnumbers(10,1);
+    // int ans = sumOfNnumbers(10,1);
     // int ans = nFactorial(5);
-    cout << ans;
+    // cout << ans;
+    cout << checkPalindrome("A man, A plan, a canal: Panama");
+    // cout << checkPalindrome(" , / ");
 
     
 }
